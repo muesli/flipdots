3e6208dd073bf55c6842441fe3311f7cf7e4bdea
Christian Muehlhaeuser <muesli@gmail.com>
2020-02-08 01:34:16 +0100

    Fix scrolling method

--- flipdots.go
+++ flipdots.go
@@ -66,10 +66,12 @@ func (fd *FlipDots) ImageToMatrix(img image.Image) string {
 
 func (fd *FlipDots) ScrollImage(img image.Image) error {
 	b := img.Bounds()
-	for x := -fd.Width; x < b.Max.X+1; x++ {
+	for x := fd.Width + 1; x > -img.Bounds().Dx(); x-- {
 		subimg := image.NewRGBA(image.Rect(0, 0, fd.Width*16, fd.Height))
-		draw.Draw(subimg, b, fd.Background, image.ZP, draw.Src)
-		draw.Draw(subimg, b, img, b.Min.Add(image.Pt(x, 0)), draw.Src)
+		draw.Draw(subimg, subimg.Bounds(), fd.Background, image.ZP, draw.Src)
+
+		r := image.Rectangle{image.Pt(x, 0), image.Pt(x, 0).Add(b.Size())}
+		draw.Draw(subimg, r, img, b.Min, draw.Src)
 
 		err := fd.SendImage(subimg)
 		if err != nil {
